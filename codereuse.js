b588ec3797573bc8a1d114489d6b3129ae913b00
Fredrick Lou <fredrick.lou@gmail.com>
2018-07-13 12:35:28 -0700

    prototypal added

--- codereuse.js
+++ codereuse.js
@@ -40,12 +40,61 @@ if(dog.energy + time < 100) {
 }
 
 //functional class
-var doglike = function(obj, name, breed, age, happiness, hunger, energy) {
-  obj.name = name;
-  obj.breed = breed;
-  obj.age = age;
-  obj.happiness = happiness;
-  obj.hunger = hunger;
-  obj.energy = energy;
+var dog = function(obj, name, breed, age, happiness, hunger, energy) {
+
+  var obj = {
+    name: name,
+    breed: breed,
+    age: age,
+    happiness: happiness,
+    hunger: hunger,
+    energy: energy,
+  }
   return obj;
+}
+
+
+
+//Prototypal
+var Dog = function(name, breed, age, happiness, hunger, energy) {
+
+  var obj = Object.create(Dog.prototype);
+  obj.name= name;
+  obj.breed= breed;
+  obj.age= age;
+  obj.happiness= happiness;
+  obj.hunger= hunger;
+  obj.energy= energy;
+
+  return obj;
+}
+
+Dog.prototype.feed = function (food) {
+if(this.hunger - food > 0) {
+  this.hunger -= food;
+} else {
+  this.hunger = 0;
+}
+}
+
+Dog.prototype.play = function(time) {
+if(this.happiness + time < 100) {
+  this.happiness += time;
+} else {
+  this.happiness = 100;
+}
+
+if(this.energy - time > 0) {
+  this.energy -= time;
+} else {
+  this.energy = 0;
+}
+}
+
+Dog.prototype.nap = function(time) {
+if(this.energy + time < 100) {
+  this.energy += time;
+} else {
+  this.energy = 100;
+}
 }
